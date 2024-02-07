56eac9e31480ea8e26cc35d5352148e25fd94e2a
Suraj Soni <suraj.soni@cctech.co.in>
2024-02-07 15:03:02 +0530

    Assignment-03

---
+++ headers/Cubiod.h
@@ -0,0 +1,16 @@
+#include<.\Point3D.h>
+#pragma once
+class Cubiod
+{
+public:
+    Cubiod();
+    Cubiod(double,double,double,Point3D);
+    ~Cubiod();
+    double getVolume();
+
+private:
+    int mLength;
+    int mBreadth;
+    int mHeight;
+    Point3D mPoint3D;
+};
---
+++ headers/Sphere.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <.\Point3D.h>
+class Sphere
+{
+public:
+    Sphere();
+    Sphere(double,Point3D);
+    ~Sphere();
+    double getVolume();
+
+private:
+    double mRadius;
+    Point3D mPoint3D;
+};
