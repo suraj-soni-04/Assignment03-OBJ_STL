2da6408b57323a5c0f7f9ca13e375832ddc5b61f
Suraj Soni <154866411+suraj-soni-04@users.noreply.github.com>
2024-02-08 19:21:19 +0530

    Update Triangulation.h

--- headers/Triangulation.h
+++ headers/Triangulation.h
@@ -1,16 +1,16 @@
 #pragma once
-#include".\Triangle.h"
-#include".\Point3D.h"
 #include<vector>
 #include<map>
+#include".\Triangle.h"
+#include".\Point3D.h"
 using namespace std;
 class Triangulation
 {
     public:
         Triangulation();
         ~Triangulation();
-        vector<Triangle>& getTriangles();
-        vector<Point3D>& getPoints();
+        vector<Triangle>& getTriangles() const;
+        vector<Point3D>& getPoints() const;
     private:
         vector<Triangle> mTraingles;
         vector<Point3D> mUnique3DPoints;
