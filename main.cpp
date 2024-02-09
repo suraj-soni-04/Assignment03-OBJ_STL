1ccfbbfb15a4449bcbfd377615c4e032e79a132c
Suraj Soni <154866411+suraj-soni-04@users.noreply.github.com>
2024-02-09 10:17:29 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <unordered_map>
-#include <thread>
 #include <chrono>
 #include <windows.h>
 #include <string.h>
@@ -17,12 +16,9 @@
 #include ".\headers\STLtoOBJ.h"
 #include ".\headers\OBJToSTL.h"
 
-
-int main()
-{
-     int choice;
-    while (1)
-    {
+int main() {
+    int choice;
+    while (true) {
         std::cout << "1. Convert File (.STL --->.OBJ)" << std::endl;
         std::cout << "2. Convert File (.OBJ --->.STL)" << std::endl;
         std::cout << "3. Open .STL file in GNUPLOT" << std::endl;
@@ -30,93 +26,93 @@ int main()
         std::cout << "5. Exit" << std::endl;
         std::cout << "Enter your choice : ";
         std::cin >> choice;
-        if (ch == 1)
-        {
-            STLtoOBJConverter stlToObjConvertor;
-            string fromStlFilePath = ".\\stlFiles\\cube.stl";
-            string toObjFilePath = ".\\convertedObjFiles\\cube.obj";
-            std::cout << "Reading '.stl' file..." << std::endl;
-            Sleep(3000);
-            std::cout << "Converting to '.obj' file" << std::endl;
-            Sleep(5000);
-            stlToObjConvertor.stlToObjConvertor(fromStlFilePath, toObjFilePath);
-            Sleep(2000);
-            std::cout << "Conversion Completed on the given path!" << std::endl;
-            std::cout << "\n";
-        }
-        else if (ch == 2)
-        {
-            OBJtoSTLConverter objToStlConvertor;
-            string fromObjFilePath = ".\\objFiles\\cube.obj";
-            string toStlFilePath = ".\\convertedStlFiles\\cube.stl";
-            std::cout << "Reading '.obj' file..." << std::endl;
-            Sleep(3000);
-            std::cout << "Converting to '.stl' file" << std::endl;
-            Sleep(5000);
-            objToStlConvertor.objToStlConvertor(fromObjFilePath, toStlFilePath);
-            Sleep(2000);
-            std::cout<<"** DONE **"<<std::endl;
-            std::cout << "Conversion Completed on the given path!" << std::endl;
-            std::cout << "\n";
-        }
-        else if (ch == 3)
-        {
-            string inputFile = ".\\stlFiles\\cube.stl";
-            string outputFile = ".\\txtFiles\\file.txt";
 
-            STLReader stlReader;
-            STLWriter stlWriter;
-            Triangulation triangulation;
+        switch (choice) {
+            case 1: {
+                STLtoOBJConverter stlToObjConvertor;
+                std::string fromStlFilePath = ".\\stlFiles\\cube.stl";
+                std::string toObjFilePath = ".\\convertedObjFiles\\cube.obj";
+                std::cout << "Reading '.stl' file..." << std::endl;
+                Sleep(3000);
+                std::cout << "Converting to '.obj' file" << std::endl;
+                Sleep(5000);
+                stlToObjConvertor.stlToObjConvertor(fromStlFilePath, toObjFilePath);
+                Sleep(2000);
+                std::cout << "Conversion Completed on the given path!" << std::endl;
+                std::cout << "\n";
+                break;
+            }
+            case 2: {
+                OBJtoSTLConverter objToStlConvertor;
+                std::string fromObjFilePath = ".\\objFiles\\cube.obj";
+                std::string toStlFilePath = ".\\convertedStlFiles\\cube.stl";
+                std::cout << "Reading '.obj' file..." << std::endl;
+                Sleep(3000);
+                std::cout << "Converting to '.stl' file" << std::endl;
+                Sleep(5000);
+                objToStlConvertor.objToStlConvertor(fromObjFilePath, toStlFilePath);
+                Sleep(2000);
+                std::cout << "** DONE **" << std::endl;
+                std::cout << "Conversion Completed on the given path!" << std::endl;
+                std::cout << "\n";
+                break;
+            }
+            case 3: {
+                std::string inputFile = ".\\stlFiles\\cube.stl";
+                std::string outputFile = ".\\txtFiles\\file.txt";
 
-            vector<Triangle> triangles = triangulation.getTriangles();
-            vector<Point3D> points = triangulation.getPoints();
+                STLReader stlReader;
+                STLWriter stlWriter;
+                Triangulation triangulation;
 
-            unordered_map<int, Point3D> mapping;
-            vector<Point3D> uniquePoints;
-            vector<Triangle> uniqueTriangles;
+                std::vector<Triangle> triangles = triangulation.getTriangles();
+                std::vector<Point3D> points = triangulation.getPoints();
 
-            std::cout << "*** STARTED ***" << std::endl;
-            Sleep(3000);
-            std::cout << "Processing the '.stl' file...." << std::endl;
-            Sleep(5000);
-            stlReader.fileReader(inputFile, points, triangles, mapping);
-            Sleep(3000);
-            std::cout << "'.stl' file processed!..." << std::endl;
-            std::cout << "Converting to '.txt' file" << std::endl;
-            Sleep(5000);
-            stlWriter.fileWriter(outputFile, points, triangles, mapping);
-            std::cout << "*** END ***" << std::endl<<std::endl;
-        }
-        else if (ch == 4)
-        {
-            string inputFile = "..\\objFiles\\cube.obj";
-            string outputFile = "..\\txtFiles\\file.txt";
+                std::unordered_map<int, Point3D> mapping;
+                std::vector<Point3D> uniquePoints;
+                std::vector<Triangle> uniqueTriangles;
 
-            ObjReader objReader;
-            ObjWriter objWriter;
-            Triangulation triangulation;
-            vector<Point3D> uniquePoints = triangulation.getPoints();
-            vector<Triangle> triangles = triangulation.getTriangles();
+                std::cout << "*** STARTED ***" << std::endl;
+                Sleep(3000);
+                std::cout << "Processing the '.stl' file...." << std::endl;
+                Sleep(5000);
+                stlReader.fileReader(inputFile, points, triangles, mapping);
+                Sleep(3000);
+                std::cout << "'.stl' file processed!..." << std::endl;
+                std::cout << "Converting to '.txt' file" << std::endl;
+                Sleep(5000);
+                stlWriter.fileWriter(outputFile, points, triangles, mapping);
+                std::cout << "*** END ***" << std::endl << std::endl;
+                break;
+            }
+            case 4: {
+                std::string inputFile = "..\\objFiles\\cube.obj";
+                std::string outputFile = "..\\txtFiles\\file.txt";
 
-            std::cout << "*** STARTED ***" << std::endl;
-            Sleep(3000);
-            std::cout << "Processing the '.obj' file...." << std::endl;
-            Sleep(5000);
-            objReader.fileReader(inputFile, uniquePoints, triangles);
-            Sleep(3000);
-            std::cout << "'.obj' file processed!..." << std::endl;
-            std::cout << "Converting to '.txt' file" << std::endl;
-            Sleep(5000);
-            objWriter.fileWriter(outputFile, uniquePoints, triangles);
-            std::cout << "*** END ***" << std::endl<<std::endl;
+                ObjReader objReader;
+                ObjWriter objWriter;
+                Triangulation triangulation;
+                std::vector<Point3D> uniquePoints = triangulation.getPoints();
+                std::vector<Triangle> triangles = triangulation.getTriangles();
 
-        }
-        else if (ch == 5) break;
-        else
-        {
-            std::cout << "Invalid Choice..." << std::endl;
-            continue;
+                std::cout << "*** STARTED ***" << std::endl;
+                Sleep(3000);
+                std::cout << "Processing the '.obj' file...." << std::endl;
+                Sleep(5000);
+                objReader.fileReader(inputFile, uniquePoints, triangles);
+                Sleep(3000);
+                std::cout << "'.obj' file processed!..." << std::endl;
+                std::cout << "Converting to '.txt' file" << std::endl;
+                Sleep(5000);
+                objWriter.fileWriter(outputFile, uniquePoints, triangles);
+                std::cout << "*** END ***" << std::endl << std::endl;
+                break;
+            }
+            case 5:
+                return 0;
+            default:
+                std::cout << "Invalid Choice..." << std::endl;
+                continue;
         }
     }
-    return 0;
 }
